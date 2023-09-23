4513a030eb320d96e048284d8b9f8e4f6ed8eb93
Aarav-J <aaravjain2007@gmail.com>
2023-09-23 16:54:05 -0400

    some changes ig
    Some project page changes.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import Spotify from "./Spotify";
-
-const Navbar = () => { 
-    return ( 
+import { useMediaQuery } from 'usehooks-ts'
+const Navbar = () => {
+    const isScrollLocking = useMediaQuery('(min-width: 1024px)')
+    return (
         <div className="md:z-10 z-0 absolute flex justify-between bg-background-primary h-32 items-center w-screen px-24">
             <span className="text-[2.625rem] font-black bg-navbarTitle bg-clip-text text-transparent">JAIN.DEV</span>
-            <Spotify/>
-            
+            {isScrollLocking ? <Spotify /> : null}
+
         </div>
     )
 }
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,43 +1,47 @@
 import SectionProps from "../type/sectionType"
-import Projects from "../components/Projects"
-import { useState } from "react"
-import ProjectPagination from "../components/projectsPagination/ProjectPagination"
-import image from "../assets/projectPage.jpg"
+// import Projects from "../components/Projects"
+// import { useState } from "react"
+// import ProjectPagination from "../components/projectsPagination/ProjectPagination"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
 const ProjectsPage: React.FC<SectionProps> = ({ headingRef }) => {
-    const projects = [
-        {
-            image: image,
-            title: "Flicks",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-            codeLink: "github.com",
-            demoLink: "google.com"
-        },
-        {
-            image: image,
-            title: "personalityBook",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-            codeLink: "github.com",
-            demoLink: "google.com"
-        },
-        {
-            image: image,
-            title: "JAIN.DEV",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-            codeLink: "github.com",
-            demoLink: "google.com"
-        },
+    // const projects = [
+    //     {
+    //         image: image,
+    //         title: "Flicks",
+    //         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    //         codeLink: "github.com",
+    //         demoLink: "google.com"
+    //     },
+    //     {
+    //         image: image,
+    //         title: "personalityBook",
+    //         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    //         codeLink: "github.com",
+    //         demoLink: "google.com"
+    //     },
+    //     {
+    //         image: image,
+    //         title: "JAIN.DEV",
+    //         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    //         codeLink: "github.com",
+    //         demoLink: "google.com"
+    //     },
 
 
-    ]
+    // ]
 
-    const [activeProject, setActiveProject] = useState(0)
+    // const [activeProject, setActiveProject] = useState(0)
     return (
         <div className="section w-screen h-screen bg-background-primary flex mt-8 md:mt-0 items-center justify-center ">
             <div className="heading inline-flex items-center flex-col gap-8">
-                <span className="font-semibold text-devPink text-3xl " ref={headingRef}>Projects</span>
+                <span className="font-semibold text-devPink text-7xl " ref={headingRef}>Projects</span>
                 <span className="text-devGrey font-normal text-3xl text-center w-1/3">Here is my showcase of everything memorable that I have created. I am always looking for ways to show off my skills, and create something exciting. </span>
-                <Projects image={projects[activeProject].image} title={projects[activeProject].title} description={projects[activeProject].description} codeLink={projects[activeProject].codeLink} demoLink={projects[activeProject].demoLink} />
-                <ProjectPagination activeProject={activeProject} setActiveProject={setActiveProject} />
+                <div className="group flex flex-row items-center justify-center gap-4 hover:cursor-pointer ">
+                    <span className="text-devPink text-3xl">Learn More</span>
+                    <FontAwesomeIcon icon={faChevronRight} className="text-devPink text-3xl group-hover:translate-x-6 transition" />
+                </div>
             </div>
         </div>
     )
