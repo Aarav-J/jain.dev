53a5abd298adb74541cd1e83cfd4a8e7db39f31f
Aarav-J <aaravjain2007@gmail.com>
2023-09-24 18:25:45 -0400

    build fix

--- src/pageTypes/mobilePage/pages/LandingPage.tsx
+++ src/pageTypes/mobilePage/pages/LandingPage.tsx
@@ -15,12 +15,11 @@ const LandingPage: React.FC<LandingPageProps> = ({ headingRef, aboutRef }) => {
         <div className={` w-screen h-screen bg-background-primary flex items-center justify-center`} ref={headingRef}>
             <div className={`heading flex items-center w-full flex-col justify-center gap-4`}>
                 <span className="font-semibold text-devPink text-3xl">Who Am I?</span>
-                <span onClick={() => {
-                    console.log("kl;jadf")
-                }} className="text-white font-black text-7xl text-center">I'm Aarav Jain</span>
+                <span className="text-white font-black text-7xl text-center">I'm Aarav Jain</span>
                 <span className="text-devGrey font-normal text-7xl lg:text-left text-center">Someone who enjoys <span className="typewriter">{word}</span></span>
                 <button className="text-xl font-normal w-32 h-12 rounded-2xl border-devPink border-2 border-solid text-white mt-2" onClick={() => {
-
+                    scrollTo(aboutRef)
+                    console.log("kl;jadf")
                 }}>Learn More</button>
             </div>
         </div>
