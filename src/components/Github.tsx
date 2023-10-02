60e31dbe0bd9ff3887c90021ff8b198bb16c79cc
Aarav-J <aaravjain2007@gmail.com>
2023-10-02 19:33:01 -0400

    favicons

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -3,7 +3,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons"
 const Github = () => {
     return (
         <div className="fixed bottom-8 right-8 z-10">
-            <a href="https://github.com/aarav-j" className="hovered"><FontAwesomeIcon icon={faGithub} className="hovered text-devGrey w-20 h-20" /></a>
+            <a href="https://github.com/aarav-j" target="_blank" className="hovered"><FontAwesomeIcon icon={faGithub} className="hovered text-devGrey w-20 h-20" /></a>
         </div>
     )
 }
--- src/pageTypes/mobilePage/components/Github.tsx
+++ src/pageTypes/mobilePage/components/Github.tsx
@@ -3,7 +3,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons"
 const Github = () => {
     return (
         <div className="fixed bottom-16 z-10">
-            <a href="https://github.com/aarav-j"><FontAwesomeIcon icon={faGithub} className="hovered text-background-primary w-20 h-20" /></a>
+            <a href="https://github.com/aarav-j" target="_blank"><FontAwesomeIcon icon={faGithub} className="hovered text-background-primary w-20 h-20" /></a>
         </div>
     )
 }
